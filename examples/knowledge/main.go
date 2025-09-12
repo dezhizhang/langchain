3a356bc5380c3016967f8bcb43727352592013a5
dezhizhang <1541609448@qq.com>
2025-09-12 11:04:02 +0800

    feat: 添加知识库

--- examples/knowledge/main.go
+++ examples/knowledge/main.go
@@ -19,55 +19,67 @@ var (
 )
 
 func main() {
+	//fb, err := os.Open("./employee.pdf")
+
 	ctx := context.Background()
 
-	fb, err := os.Open("./employee.pdf")
+	f, err := os.Open("./employee.pdf")
 	if err != nil {
 		panic(err)
 	}
-	defer fb.Close()
-	content, err := fb.Stat()
-	handleError(err)
 
-	p := documentloaders.NewPDF(fb, content.Size())
-	split, err := p.LoadAndSplit(
-		ctx,
-		textsplitter.NewRecursiveCharacter(
-			textsplitter.WithChunkSize(200),
-			textsplitter.WithChunkOverlap(1),
-		),
-	)
+	defer f.Close()
 
-	handleError(err)
+	stat, err := f.Stat()
+	if err != nil {
+		panic(err)
+	}
 
-	// 创建embedding
+	// 将文件进行加载
+	p := documentloaders.NewPDF(f, stat.Size())
+
+	// 将文件进行切分
+	chunk, err := p.LoadAndSplit(ctx, textsplitter.NewRecursiveCharacter(
+		textsplitter.WithChunkSize(200),
+		textsplitter.WithChunkOverlap(1),
+	))
+	if err != nil {
+		panic(err)
+	}
+
+	// 创建大模型
 	llm, err := openai.New(openai.WithToken(apiKey), openai.WithBaseURL(url))
-	handleError(err)
+	if err != nil {
+		panic(err)
+	}
 
 	embedder, err := embeddings.NewEmbedder(llm)
-	handleError(err)
+	if err != nil {
+		panic(err)
+	}
 
-	// 设置向量数据库
-	store, err := redisvector.New(ctx,
+	// 介建向量数据库
+	store, err := redisvector.New(
+		ctx,
 		redisvector.WithEmbedder(embedder),
 		redisvector.WithConnectionURL("redis://106.15.74.79:6379"),
 		redisvector.WithIndexName("knowledge", true),
 	)
-	handleError(err)
-	_, err = store.AddDocuments(ctx, split)
-	handleError(err)
-
-	qa := chains.NewRetrievalQAFromLLM(llm, vectorstores.ToRetriever(store, 1))
-
-	rsp, err := chains.Run(ctx, qa, "公司的考勤是怎样的")
-	handleError(err)
-
-	fmt.Println(rsp)
-
-}
-
-func handleError(err error) {
 	if err != nil {
 		panic(err)
 	}
+
+	_, err = store.AddDocuments(ctx, chunk)
+	if err != nil {
+		panic(err)
+	}
+
+	// 用户发起提问
+	qa := chains.NewRetrievalQAFromLLM(llm, vectorstores.ToRetriever(store, 1))
+	res, err := chains.Run(ctx, qa, "工作时间与考勤")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("res=", res)
+
 }
--- examples/langchain/langchain.go
+++ examples/langchain/langchain.go
@@ -13,36 +13,19 @@ var (
 )
 
 func main() {
-	//llm, err := openai.New(openai.WithToken(apiKey), openai.WithBaseURL(url))
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//text, err := llms.GenerateFromSinglePrompt(context.Background(), llm, "贵州数擎科技目前做什么")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//fmt.Println(text)
 
 	llm, err := openai.New(openai.WithToken(apiKey), openai.WithBaseURL(url))
 	if err != nil {
 		panic(err)
 	}
 
-	text, err := llms.GenerateFromSinglePrompt(context.Background(), llm, "贵州数擎科技目前做什么")
+	rsp, err := llm.GenerateContent(context.Background(), []llms.MessageContent{
+		llms.TextParts(llms.ChatMessageTypeSystem, "你是一个诗人"),
+		llms.TextParts(llms.ChatMessageTypeHuman, "请用诗描述我很帅气"),
+	})
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(text)
-
-	//rsp, err := llm.GenerateContent(context.Background(), []llms.MessageContent{
-	//	llms.TextParts(llms.ChatMessageTypeSystem, "我是贵州数擎科技有限公司"),
-	//	llms.TextParts(llms.ChatMessageTypeHuman, "请说出你需要的帮助"),
-	//})
-	//if err != nil {
-	//	panic(err)
-	//}
-	//fmt.Println(rsp.Choices[0].Content)
+	fmt.Println(rsp.Choices[0].Content)
 
 }
